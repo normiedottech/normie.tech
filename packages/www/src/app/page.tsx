ed094578ccd88872920cfe05ab22b7e560c497f4
dipanshuhappy <dipanshuhappy@gmail.com>
2024-11-21 15:23:47 +0530

    Fix indentation for AryanPage component in Home function

--- packages/www/src/app/page.tsx
+++ packages/www/src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <>
       <PostHogFeature match={variants.Control} flag={FEATURE_FLAG}>
-      <AryanPage />
+        <AryanPage />
       </PostHogFeature>
       <PostHogFeature match={variants.Swaraj} flag={FEATURE_FLAG}>
         <AryanPage />
