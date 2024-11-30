5d0145a492789cbb72e16b6fb315f325f419734e
dipanshuhappy <dipanshuhappy@gmail.com>
2024-12-01 00:05:41 +0530

    Update base URL for normieTechClient to new API endpoint

--- packages/www/src/lib/normie-tech.ts
+++ packages/www/src/lib/normie-tech.ts
@@ -386,7 +386,7 @@ export type operations = Record<string, never>;
 
 
 export const normieTechClient = createClient<paths>({
-  baseUrl: 'https://84i54kd5nk.execute-api.us-east-1.amazonaws.com',
+  baseUrl: 'https://api.normie.tech',
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
