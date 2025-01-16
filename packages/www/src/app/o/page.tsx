c5cfe3712018ecbaa212795223baa948682eb952
dipanshuhappy <dipanshuhappy@gmail.com>
2025-01-17 00:04:39 +0530

    Add redirect page to navigate to payment link

---
+++ packages/www/src/app/o/page.tsx
@@ -0,0 +1,16 @@
+// https://normie.tech/pay/Jy8DAwmoq2
+
+
+import { redirect } from "next/navigation";
+
+const Page = () => {
+
+
+
+
+    
+    redirect("https://normie.tech/pay/Jy8DAwmoq2")
+
+}
+
+export default Page;
